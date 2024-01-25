890a50fd68ae5f49b4279f80ef967586d488d9b7
YordanDobrev <petolisko@gmail.com>
2024-01-25 11:24:15 +0200

    Task is ready

--- 8. Multidimensional Lists - Exercise 2/03_knight_game.py
+++ 8. Multidimensional Lists - Exercise 2/03_knight_game.py
@@ -2,7 +2,7 @@ board = int(input())
 
 matrix = [[x for x in input()] for element in range(board)]
 
-knight_score = 0
+knight_removed = 0
 
 positions = (
     (-2, -1),  # up -2, left -1
@@ -14,9 +14,32 @@ positions = (
     (1, 2),  # down 1, right 2
     (2, 1),  # down 2, right 1
 )
+while True:
+    max_attacks = 0
+    knight_max_pos = []
 
+    for row_index in range(board):
+        for col_index in range(board):
+            if matrix[row_index][col_index] == "K":
+                attacks = 0
 
+                for pos in positions:
+                    position_r = row_index + pos[0]
+                    position_c = col_index + pos[1]
 
+                    if 0 <= position_r < board and 0 <= position_c < board:
+                        if matrix[position_r][position_c] == "K":
+                            attacks += 1
 
+                if attacks > max_attacks:
+                    max_attacks = attacks
+                    knight_max_pos = [row_index, col_index]
 
-[print(*index, sep="") for index in matrix]
+    if knight_max_pos:
+        row, col = knight_max_pos
+        matrix[row][col] = "0"
+        knight_removed += 1
+    else:
+        break
+
+print(knight_removed)
