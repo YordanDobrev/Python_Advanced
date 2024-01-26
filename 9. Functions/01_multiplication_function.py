f945f3f373f98fe88bb16382aed1bdc2e53f9d77
YordanDobrev <petolisko@gmail.com>
2024-01-26 07:39:11 +0200

    Task is ready

---
+++ 9. Functions/01_multiplication_function.py
@@ -0,0 +1,11 @@
+def multiply(*args):
+    total = 1
+
+    for index in args:
+        total *= index
+    return total
+
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
