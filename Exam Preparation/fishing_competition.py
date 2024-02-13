c4f565e62ef5cde2fd702cd5c404d0669c644d7f
YordanDobrev <petolisko@gmail.com>
2024-02-13 14:20:54 +0200

    Task is ready

--- Exam Preparation/fishing_competition.py
+++ Exam Preparation/fishing_competition.py
@@ -1,36 +1,72 @@
-num = int(input())
+def movement(directions, command, matrix, r, c):
+    path = directions[command]
 
-matrix = [[x for x in input()] for element in range(num)]
+    next_row = r + path[0]
+    next_col = c + path[1]
 
-ship_coordinates = []
-fish_amount = 0
+    if next_row < 0:
+        next_row = len(matrix) - 1
+    elif next_row >= len(matrix):
+        next_row = 0
 
-for row in matrix:
-    for column in row:
-        if "S" in column:
-            ship_coordinates = [column.index("S"), row.index("S")]
-            matrix[ship_coordinates[0]][ship_coordinates[1]] = "-"
+    if next_col < 0:
+        next_col = len(matrix) - 1
+    elif next_col >= len(matrix):
+        next_col = 0
 
-print(ship_coordinates)
-print(*matrix, sep="\n")
+    return next_row, next_col
 
-positions = (
-    (-1, 0),  # up
-    (1, 0),  # down
-    (0, -1),  # left
-    (0, 1),  # right
-)
+
+size = int(input())
+
+matrix = []
+ship_cords = []
+
+for index in range(size):
+    line = list(input())
+
+    if "S" in line:
+        ship_cords = [index, line.index("S")]
+        line[ship_cords[1]] = "-"
+    matrix.append(line)
+
+directions = {
+    "up": (-1, 0),
+    "down": (1, 0),
+    "left": (0, -1),
+    "right": (0, 1),
+}
+
+fish = 0
 
 command = input()
+latest_ship_cords = ship_cords
 
 while command != "collect the nets":
+    r, c = latest_ship_cords
+    row, col = movement(directions, command, matrix, r, c)
+    latest_ship_cords = [row, col]
+    next_symbol = matrix[row][col]
 
-    for row_index in range(num):
-        for col_index in range(num):
-
-            for pos in positions:
-                r = row_index + pos[0]
-                c = col_index + pos[1]
-
-
+    if next_symbol.isdigit():
+        fish += int(matrix[row][col])
+        matrix[row][col] = "-"
+    elif next_symbol == "W":
+        print(
+            f"You fell into a whirlpool! The ship sank and you lost the fish you caught. "
+            f"Last coordinates of the ship: [{','.join([str(el)for el in latest_ship_cords])}]")
+        exit()
     command = input()
+
+matrix[latest_ship_cords[0]][latest_ship_cords[1]] = "S"
+
+if fish >= 20:
+    print(f"Success! You managed to reach the quota!")
+else:
+    print(f"You didn't catch enough fish and didn't reach the quota! You need {20 - fish} tons of fish more.")
+
+if fish > 0:
+    print(f"Amount of fish caught: {fish} tons.")
+
+for i in matrix:
+    print(''.join(i))
